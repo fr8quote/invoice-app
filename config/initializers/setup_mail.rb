676620b696bdb5ba2df5d52a8a609bc0712cd345
chezzie <info@fr8quote.co>
2015-12-07 17:06:53 +0000

    first commit

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address              =>  'smtp.gmail.com',
+  :port                 =>  '587',
+  :authentication       =>  :plain,
+  :user_name            =>  'fr8quoteinc@gmail.com',
+  :password             =>  'Kat1leidy',
+  :domain               =>  'gmail.com',
+  :enable_starttls_auto  =>  true 
+}
